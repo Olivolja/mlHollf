5603a2570a59a4591de3d9150e0836e62ab6a1db
Olivolja <adamwoliv@gmail.com>
2020-03-03 09:45:42 +0100

    added bash example file to new branch

---
+++ bash.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+#SBATCH -A SNIC009-87-654 -p hebbe
+#SBATCH -n 10
+#SBATCH -t 0-00:10:00
+
+#LOAD MODULES HERE
+
+
+#DO STUFF HERE
+
+echo "Hello cluster computing world!"
+sleep 60 
