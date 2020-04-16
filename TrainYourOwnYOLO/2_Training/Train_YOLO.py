05c52bfb426efc8bafcad5643e338608c5fb5f14
Vinnerholt <benvin@student.chalmers.se>
2020-04-16 17:35:06 +0200

    Small changes to run the checkpoint-weights and batch size 32

--- TrainYourOwnYOLO/2_Training/Train_YOLO.py
+++ TrainYourOwnYOLO/2_Training/Train_YOLO.py
@@ -197,7 +197,7 @@ if __name__ == "__main__":
             },
         )
 
-        batch_size = 2
+        batch_size = 32
         print(
             "Train on {} samples, val on {} samples, with batch size {}.".format(
                 num_train, num_val, batch_size
